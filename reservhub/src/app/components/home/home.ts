b8e822445c00af94fa456ced70522817777a8673
Kay <118884667+BadRix90@users.noreply.github.com>
2025-08-05 11:30:48 +0200

    create navbar

--- reservhub/src/app/components/home/home.ts
+++ reservhub/src/app/components/home/home.ts
@@ -5,10 +5,12 @@ import { About } from '../about/about';
 import { Documents } from '../documents/documents';
 import { Links } from '../links/links';
 import { Footer } from '../footer/footer';
+import { Contact } from "../contact/contact";
+import { Navbar } from "../navbar/navbar";
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, About, Documents, Links, Footer],
+  imports: [Hero, About, Documents, Links, Footer, Contact, Navbar],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
--- reservhub/src/app/components/navbar/navbar.ts
+++ reservhub/src/app/components/navbar/navbar.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
 export class Navbar {
-
+  protected readonly isMenuOpen = signal(false);
+  
+  protected readonly navItems = signal([
+    { label: 'Info', path: '/about', fragment: undefined },
+    { label: 'Dokumente', path: '/documents', fragment: undefined },
+    { label: 'Links', path: '/links', fragment: undefined },
+    { label: 'Kontakt', path: '/contact', fragment: undefined }
+  ]);
+  
+  protected toggleMenu(): void {
+    this.isMenuOpen.update(value => !value);
+  }
+  
+  protected closeMenu(): void {
+    this.isMenuOpen.set(false);
+  }
 }
