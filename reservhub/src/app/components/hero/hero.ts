21358ce067667f1eba6e4a58145f29ba9759738a
Kay <118884667+BadRix90@users.noreply.github.com>
2025-08-05 14:31:07 +0200

    create new colors

--- reservhub/src/app/components/hero/hero.ts
+++ reservhub/src/app/components/hero/hero.ts
@@ -1,8 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { Navbar } from '../navbar/navbar';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.html',
+  imports: [Navbar],
   styleUrl: './hero.scss'
 })
 export class Hero {
