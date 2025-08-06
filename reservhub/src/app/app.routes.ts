85927911fb66780fa465c5a8703f8cbaf97bfb1b
Kay <118884667+BadRix90@users.noreply.github.com>
2025-08-06 10:30:55 +0200

    add privacy and legal-notice

--- reservhub/src/app/app.routes.ts
+++ reservhub/src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { About } from './components/about/about';
 import { Documents } from './components/documents/documents';
+import { PrivacyPolicy } from './components/privacy-policy/privacy-policy';
+import { LegalNotice } from './components/legal-notice/legal-notice';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'home', redirectTo: '', pathMatch: 'full' },
   { path: 'about', component: About },
   { path: 'documents', component: Documents },
+  { path: 'privacy-policy', component: PrivacyPolicy },
+  { path: 'legal-notice', component: LegalNotice },
   { path: '**', redirectTo: '' }
 ];
